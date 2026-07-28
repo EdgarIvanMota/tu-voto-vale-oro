6c272f4dc3878c3f1e533ad88ac1a94d24ba6aab
EdgarIvanMota <edgar.mota23ids@nube.unadmexico.mx>
2026-07-27 18:59:33 -0600

    Remove chatbot open/close event listeners

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,15 +1,1 @@
-const abrir = document.getElementById("abrirChat");
-const cerrar = document.getElementById("cerrarChat");
-const chatbot = document.getElementById("chatbot");
 
-abrir.addEventListener("click", function(){
-
-    chatbot.style.display = "flex";
-
-});
-
-cerrar.addEventListener("click", function(){
-
-    chatbot.style.display = "none";
-
-});
